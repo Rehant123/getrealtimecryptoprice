7fe792376e6f57e9ec47d9af92881e796bbc0cac
Rehant123 <rehantthapa.chelsea@gmail.com>
2024-01-27 21:53:54 +0530

    just made a new branch

--- src/Components/CoinTable.js
+++ src/Components/CoinTable.js
@@ -17,98 +17,108 @@ const [Loading,setLoading] = useState(false)
 const [search,setSearch] = useState("");
 
 const [coins,setCoins] = useState([]);
-const data = [
-  {
-    "id": "bitcoin",
-    "symbol": "btc",
-    "name": "Bitcoin",
-    "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
-    "current_price": 41431,
-    "market_cap": 811918115588,
-    "price_change_percentage_24h": 4.32546
-  },
-  {
-    "id": "ethereum",
-    "symbol": "eth",
-    "name": "Ethereum",
-    "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
-    "current_price": 2259.41,
-    "market_cap": 271663611201,
-    "price_change_percentage_24h": 3.40906
-  },
-  {
-    "id": "tether",
-    "symbol": "usdt",
-    "name": "Tether",
-    "image": "https://assets.coingecko.com/coins/images/325/large/Tether.png?1696501661",
-    "current_price": 1.001,
-    "market_cap": 95666520205,
-    "price_change_percentage_24h": 0.02606
-  },
-  {
-    "id": "binancecoin",
-    "symbol": "bnb",
-    "name": "BNB",
-    "image": "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970",
-    "current_price": 302.47,
-    "market_cap": 46669690770,
-    "price_change_percentage_24h": 3.53901
-  },
-  {
-    "id": "solana",
-    "symbol": "sol",
-    "name": "Solana",
-    "image": "https://assets.coingecko.com/coins/images/4128/large/solana.png?1696504756",
-    "current_price": 92.45,
-    "market_cap": 40074455794,
-    "price_change_percentage_24h": 7.01119
-  },
-  {
-    "id": "ripple",
-    "symbol": "xrp",
-    "name": "XRP",
-    "image": "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1696501442",
-    "current_price": 0.521575,
-    "market_cap": 28352722435,
-    "price_change_percentage_24h": 2.97753
-  },
-  {
-    "id": "usd-coin",
-    "symbol": "usdc",
-    "name": "USDC",
-    "image": "https://assets.coingecko.com/coins/images/6319/large/usdc.png?1696506694",
-    "current_price": 1.001,
-    "market_cap": 26047832916,
-    "price_change_percentage_24h": 0.3111
-  },
-  {
-    "id": "staked-ether",
-    "symbol": "steth",
-    "name": "Lido Staked Ether",
-    "image": "https://assets.coingecko.com/coins/images/13442/large/steth_logo.png?1696513206",
-    "current_price": 2257.61,
-    "market_cap": 21135693739,
-    "price_change_percentage_24h": 3.44814
-  },
-  {
-    "id": "cardano",
-    "symbol": "ada",
-    "name": "Cardano",
-    "image": "https://assets.coingecko.com/coins/images/975/large/cardano.png?1696502090",
-    "current_price": 0.485972,
-    "market_cap": 17051564381,
-    "price_change_percentage_24h": 4.32334
-  },
-  {
-    "id": "polkadot",
-    "symbol": "dot",
-    "name": "Polkadot",
-    "image": "https://assets.coingecko.com/coins/images/12171/large/aJGBjJFU_400x400.jpg?1597804776",
-    "current_price": 23.45,
-    "market_cap": 22156937897,
-    "price_change_percentage_24h": 2.345
-  }
-]
+
+const fetchCoins = ()=>{
+
+  setCoins([
+    {
+      "id": "bitcoin",
+      "symbol": "btc",
+      "name": "Bitcoin",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+      "current_price": 41431,
+      "market_cap": 811918115588,
+      "price_change_percentage_24h": 4.32546
+    },
+    {
+      "id": "ethereum",
+      "symbol": "eth",
+      "name": "Ethereum",
+      "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
+      "current_price": 2259.41,
+      "market_cap": 271663611201,
+      "price_change_percentage_24h": 3.40906
+    },
+    {
+      "id": "tether",
+      "symbol": "usdt",
+      "name": "Tether",
+      "image": "https://assets.coingecko.com/coins/images/325/large/Tether.png?1696501661",
+      "current_price": 1.001,
+      "market_cap": 95666520205,
+      "price_change_percentage_24h": 0.02606
+    },
+    {
+      "id": "binancecoin",
+      "symbol": "bnb",
+      "name": "BNB",
+      "image": "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1696501970",
+      "current_price": 302.47,
+      "market_cap": 46669690770,
+      "price_change_percentage_24h": 3.53901
+    },
+    {
+      "id": "solana",
+      "symbol": "sol",
+      "name": "Solana",
+      "image": "https://assets.coingecko.com/coins/images/4128/large/solana.png?1696504756",
+      "current_price": 92.45,
+      "market_cap": 40074455794,
+      "price_change_percentage_24h": 7.01119
+    },
+    {
+      "id": "ripple",
+      "symbol": "xrp",
+      "name": "XRP",
+      "image": "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1696501442",
+      "current_price": 0.521575,
+      "market_cap": 28352722435,
+      "price_change_percentage_24h": 2.97753
+    },
+    {
+      "id": "usd-coin",
+      "symbol": "usdc",
+      "name": "USDC",
+      "image": "https://assets.coingecko.com/coins/images/6319/large/usdc.png?1696506694",
+      "current_price": 1.001,
+      "market_cap": 26047832916,
+      "price_change_percentage_24h": 0.3111
+    },
+    {
+      "id": "staked-ether",
+      "symbol": "steth",
+      "name": "Lido Staked Ether",
+      "image": "https://assets.coingecko.com/coins/images/13442/large/steth_logo.png?1696513206",
+      "current_price": 2257.61,
+      "market_cap": 21135693739,
+      "price_change_percentage_24h": 3.44814
+    },
+    {
+      "id": "cardano",
+      "symbol": "ada",
+      "name": "Cardano",
+      "image": "https://assets.coingecko.com/coins/images/975/large/cardano.png?1696502090",
+      "current_price": 0.485972,
+      "market_cap": 17051564381,
+      "price_change_percentage_24h": 4.32334
+    },
+    {
+      "id": "polkadot",
+      "symbol": "dot",
+      "name": "Polkadot",
+      "image": "https://assets.coingecko.com/coins/images/12171/large/aJGBjJFU_400x400.jpg?1597804776",
+      "current_price": 23.45,
+      "market_cap": 22156937897,
+      "price_change_percentage_24h": 2.345
+    }
+  ])
+  
+}
+
+useEffect(()=>{
+fetchCoins()
+},[currency])
+
 
 const handleSearch = ()=>{
   return coins.filter((coin)=>coin.name.toLowerCase().includes(search)||coin.symbol.toLowerCase().includes(search))
